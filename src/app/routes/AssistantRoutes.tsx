a80139f0d6920b401f18099b9ce82a63289f2be7
HStiven <hernandostivencortes@gmail.com>
2025-11-11 11:30:55 -0500

    ---

--- src/app/routes/AssistantRoutes.tsx
+++ src/app/routes/AssistantRoutes.tsx
@@ -2,9 +2,10 @@ import { Outlet } from "react-router-dom"
 import { NewDropDown } from "../components/NewDropdown"
 import useMediaQueryScreen from "../hooks/MediaScreen/useMediaQueryScreen"
 
-const [isxSmall, isSmall] = useMediaQueryScreen()
-
 const AssistantRoutes = () => {
+
+  const [isxSmall, isSmall] = useMediaQueryScreen();
+
   return (
     <section className="w-full d-flex flex-column flex-root">
       {(isxSmall || isSmall) ? <></> :
